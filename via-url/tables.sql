d2498b9ab839d394da6d38b91b048c0e2521af25
itchief <alex.malcev1980@gmail.com>
2021-10-09 16:43:11 +1000

    Update

---
+++ via-url/tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `star_rating` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `rating_id` varchar(20) NOT NULL,
+  `rating_avg` float NOT NULL,
+  `total_votes` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `star_rating_ip` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `rating_id` int(10) UNSIGNED NOT NULL,
+  `rating_value` tinyint(2) UNSIGNED NOT NULL,
+  `rating_ip` varchar(16) NOT NULL DEFAULT '0.0.0.0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `star_rating`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `star_rating_ip`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rating_id` (`rating_id`);
+
+ALTER TABLE `star_rating`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `star_rating_ip`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
+++ with_aggregaterating/README.md
@@ -0,0 +1,2 @@
+# PHP script star rating for the site
+This script is rated as stars, built on PHP and MySQL, and works without refreshing the page.
